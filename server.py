a9bad757252c18bfe01c77eece1cca9d3d0c895a
I Am Emptiness <145884361+UnknownHat832553@users.noreply.github.com>
2025-04-10 10:03:08 +0700

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Admin Bot is Running! >>> Discord: https://discord.gg/SErSdUxtTQ <<<"
+    return ">>> Bot is Running! <<<"
 
 def run():
   app.run(host='0.0.0.0',port=8080)
