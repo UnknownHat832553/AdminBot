902d5350e2d670377fbfb27f907a5169c35ffbde
I Am Emptiness <145884361+UnknownHat832553@users.noreply.github.com>
2025-04-13 11:45:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,12 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 logger = logging.getLogger('discord_bot')
 
-commands_path = "./commands"
-for filename in os.listdir(commands_path):
-    if filename.endswith(".py"):
-        bot.load_extension(f"commands.{filename[:-3]}")
-
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=nextcord.Game(name="เป็นผู้ดูสถานะเซิร์ฟเวอร์ค่ะ"))
     print('Bot Run!')
+
+bot.load_extension("ServerStatus")
 
 server_on()
 bot.run(os.getenv('TOKEN'))
